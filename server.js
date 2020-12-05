6596d2f2c4f32bc40455d50c99bb6dca85c30a77
Alan Tran <alantran96@gmail.com>
2020-12-05 02:28:45 -0500

    Test fix from youtube vid

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ const PORT = process.env.PORT || 4000
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'frontend/public')))
-
 app.use('/', router)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/public/index.html'))
-})
+if(process.env.NODE_ENV === 'production') {
+  // Server any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  //Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
