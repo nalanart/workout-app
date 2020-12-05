ee8703264f51e0da23356ec2e11651b1cc7254e2
Alan Tran <alantran96@gmail.com>
2020-12-05 01:45:24 -0500

    Fix previous commit - placed catchall into server.js instead of router.js

--- api/router.js
+++ api/router.js
@@ -2,18 +2,13 @@ const router = require('express').Router()
 const exercisesRouter = require('./exercises')
 const historyRouter = require('./history')
 const workoutsRouter = require('./workouts')
-const path = require('path')
 
 router.use('/history', historyRouter)
 router.use('/exercises', exercisesRouter)
 router.use('/workouts', workoutsRouter)
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/public/index.html'))
+router.get('/', (req, res) => {
+  res.send('Home page')
 })
-
-// router.get('/', (req, res) => {
-//   res.send('Home page')
-// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./api/router')
+const path = require('path')
 
 const PORT = process.env.PORT || 4000
 
@@ -10,6 +11,10 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/', router)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/frontend/public/index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
