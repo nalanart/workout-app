b83479ff40095f1fdc963720c11c46c2ad0129cf
Alan Tran <alantran96@gmail.com>
2020-12-05 01:55:31 -0500

    Try fix to push to heroku

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const PORT = process.env.PORT || 4000
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use(express.static('frontend/public'))
+
 app.use('/', router)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/public/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/public/index.html'))
 })
 
 app.listen(PORT, () => {
