2d9c6d99fa18d8bfb833f87198713ea6f5f5bf32
Alan Tran <alantran96@gmail.com>
2020-12-13 12:12:02 -0500

    Add active tab class to nav links

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,27 +1,32 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function NavBar() {
+  const [page, setPage] = useState('overview')
+
+  const getPageClass = pageOption => pageOption === page ? 'nav-link active' : 'nav-link'
+
   return (
     <nav className="NavBar bg-light">
       <ul className="nav justify-content-center nav-tabs">
         <li className="nav-item">
-          <Link to="/overview" className="nav-link">
+          <Link to="/overview" className={getPageClass('overview')} onClick={() => setPage('overview')}>
             Overview
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/plan" className="nav-link">
+          <Link to="/plan" className={getPageClass('plan')} onClick={() => setPage('plan')}>
             Plan
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/workout" className="nav-link">
+          <Link to="/workout" className={getPageClass('currentWorkout')} onClick={() => setPage('currentWorkout')}>
             Current Workout
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/history" className="nav-link">
+          <Link to="/history" className={getPageClass('history')} onClick={() => setPage('history')}>
             History
           </Link>
         </li>
